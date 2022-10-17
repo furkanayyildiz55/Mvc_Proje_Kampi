e8a63af31cc2581adcdc17ae7806a15cd867eec1
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2022-10-17 15:48:01 +0300

    17.10.2022

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal _AboutDal)
+        {
+            _aboutDal = _AboutDal;
+        }
+
+        public void AboutAdd(About about)
+        {
+            _aboutDal.Insert(about);
+        }
+
+        public void AboutDelete(About about)
+        {
+            _aboutDal.Delete(about);
+        }
+
+        public void AboutUpdate(About about)
+        {
+            _aboutDal.Update(about);
+        }
+
+        public About GetById(int id)
+        {
+            var aboutValue = _aboutDal.Get(x =>x.AboutID == id);
+            return aboutValue;
+        }
+
+        public List<About> GetList()
+        {
+            return _aboutDal.List();
+        }
+    }
+}
--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -57,5 +57,37 @@ namespace MvcProjeKampi.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            //DROPDOWN İÇİN 
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.category = valueCategory;
+
+            var HeadingValue = hm.GetByID(id);
+            return View(HeadingValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditHeading(Heading heading)
+        {
+            hm.HeadingUpdate(heading);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteHeading(int id)
+        {
+            var HeadingValue = hm.GetByID(id);
+            HeadingValue.HeadingStatus = !HeadingValue.HeadingStatus;
+            hm.HeadingDelete(HeadingValue);
+            return RedirectToAction("Index");
+        }
+
     }
 }
