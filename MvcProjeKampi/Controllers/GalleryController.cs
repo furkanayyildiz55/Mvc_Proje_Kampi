dcce138e2b469aba0b6a3f34e044714a924a278c
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2022-10-26 23:22:25 +0300

    26.10.2022

---
+++ MvcProjeKampi/Controllers/GalleryController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class GalleryController : Controller
+    {
+        ImageFileManager ImageFileManager = new ImageFileManager(new EfImageFileDal());
+
+        // GET: Gallery
+        public ActionResult Index()
+        {
+
+            var ImageLıst = ImageFileManager.GetList();
+            return View(ImageLıst);
+        }
+    }
+}
